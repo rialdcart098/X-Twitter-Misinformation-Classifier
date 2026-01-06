8cb01998982606c69cec0edd310592007cdc6f71
rialdcart098 <yourlocalfifa@gmail.com>
2026-01-05 20:08:13 -0800

    frontend maintenance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,34 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [link, setLink] = useState('')
+    const classify = async e => {
+        e.preventDefault()
+        axios.post('http://localhost:8000/classify', { link: link })
+    }
+    return (
+        <div>
+            <h1>Twitter Misinformation Classifier</h1>
+            <form onSubmit={classify}>
+                <input type="text" placeholder="Link" />
+                <button type='submit'>Classify</button>
+            </form>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        </div>
+    )
 }
 
 export default App
